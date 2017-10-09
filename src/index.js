c4394cec03035bff8bd21af8e187a7f5b78aec5c
Gareth Slinn <gareth.slinn@dugout.com>
2017-10-09 22:33:13 +0100

    apply routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import AddUsers from './components/AddUsers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -10,7 +11,9 @@ const store = createStore(reducer);
 
 ReactDom.render(
     <Provider store={ store }>
-        <AddUsers />
+        <Router path='/' history={browserHistory}>
+            <Route path='/' component={AddUsers} />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
