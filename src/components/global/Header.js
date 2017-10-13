1f373d4f63ae6a25b48e22b81a677f05ae323016
Gareth Slinn <gareth.slinn@dugout.com>
2017-10-13 18:27:38 +0100

    apply updates

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div className="clearfix">
-                <h1 className="float-left title">Project Tracker</h1>
+            <div>
+                <div className="clearfix">
+                    <h5>Work in progress...</h5>
+                </div>
+                <div className="clearfix">
+                    <h1 className="float-left title">Project Tracker</h1>
+                </div>
             </div>
         )
     }
